a9adccd623968d5bc0bb8f069d55e5a9d8f7ebc5
RichardJActon <rjactonspsfcf@gmail.com>
2017-10-23 16:36:12 +0100

    updated downloads page with new links

--- hsm/downloads.php
+++ hsm/downloads.php
@@ -17,17 +17,31 @@
 				<h1>Downloads</h1>
 			</div>
 		</div>
-
 		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+			<h2>Results For Individual Loci</h2>
 			<ul>
-				<li><a href="./data/Supplementary_File1_mergeFinal_hsm.bed">Replicating HSM peaks</a></li>
 				<li><a href="download.php?type=dataFiles">All Data files - by SNP</a></li>
 				<!--<li><a href="./data/dataFilesChr/">All Data files - by Chromosome</a></li>-->
 				<li><a href="download.php?type=pValPosGraphs">p-Value Graphs (.png) - by SNP</a></li>
 				<li><a href="download.php?type=pValGraphs_pdf">p-Value Graphs (.pdf) - by SNP</a></li>
 			</ul>
 		</div>
-		
+		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+			<h2>Whole Dataset Files</h2>
+			<ul>
+				<li><a href="./data/Supplementary_File1_mergeFinal_hsm.bed">Replicating HSM peaks</a></li>
+				<li><a href="./data/hg19_500bp_NonSNPVariants.bed.gz">Variant Annotations</a></li>
+			</ul>
+		</div>		
+		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+			<h2>Figures</h2>
+			<ul>
+				<li><a href="./img/hsm_legend.png">Variant Types Legend</a></li>
+				<li><a href="./img/hsm_legend_extended.png">Variant Types Legend - Extended</a></li>
+				<li><a href="./img/Bell2017Fig1_StudyDesign.png">Study Design summary figure (.png)</a></li>
+				<li><a href="./img/Bell2017Fig1_StudyDesign.pdf">Study Design summary figure (.pdf)</a></li>
+			</ul>
+		</div>
 	</div>
 </div>
 
