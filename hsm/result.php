122931b8a63a1d287e1c1f75688e1018b4bd6ff1
RichardJActon <rjactonspsfcf@gmail.com>
2017-12-01 16:12:25 +0000

    dataset indicator

--- hsm/result.php
+++ hsm/result.php
@@ -99,7 +99,7 @@
 					<div class="panel panel-default">
 						<div class="panel-heading">
 								<h2 class="panel-title">
-									<a data-toggle="collapse" href="#download">Datasets</a> <span class="badge">'.$dataset.'</span>
+									<a data-toggle="collapse" href="#download">Datasets</a>
 								</h2>
 						</div>
 						<div id="download" class="panel-collapse collapse in">
